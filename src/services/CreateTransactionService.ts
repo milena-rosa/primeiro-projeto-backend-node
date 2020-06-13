bc05415af948e13fe2032e7b9118c199b8b6ab33
Milena Boselli Rosa <milena.rosa@gmail.com>
2020-06-13 15:42:05 -0300

    feature:adicionadas funcionalidades de criaçao de transactions e listagem de transactions e balance. Utilizando principios de arquitetura de software SOLID e do Data Mapper Pattern.

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: Request): Transaction {
+    if (type === 'outcome') {
+      const balance = this.transactionsRepository.getBalance();
+      if (value > balance.total) {
+        throw Error(
+          'It is not possible to create an outcome transaction if you are in debt.',
+        );
+      }
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
